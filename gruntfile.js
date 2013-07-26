7e5411e8f809e235cd1d5ddb600e5ca01cc8282f
Burke Holland <burkeholland@gmail.com>
2013-07-26 13:24:01 -0500

    Get build process up-to-date with master

--- gruntfile.js
+++ gruntfile.js
@@ -1,65 +1,69 @@
+
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      config: {
-        options: {
-          banner: "(function(angular) {",
-          footer: "}(angular));"
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            config: {
+                options: {
+                    banner: "(function(angular) {",
+                    footer: "}(angular));"
+                }
+            },
+            dist: {
+                src: ['src/main.js', 'src/services/widget.js', 'src/factories/widget.js', 'src/factories/directive.js', 'src/directives/widgets.js', 'src/directives/dataSource.js'],
+                dest: 'build/angular-kendo.js'
+            },
+
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'build/angular-kendo.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        copy: {
+            main: {
+                files: [{
+                    src: ['docs/angular-kendo.html'],
+                    dest: 'app/partials/docs.html',
+                    filter: 'isFile'
+                }, // includes files in path
+                ]
+            }
+        },
+        watch: {
+            scripts: {
+                files: "src/**/*.js",
+                tasks: ['default']
+            }
+        },
+        changelog: {
+            options: {
+                dest: 'CHANGELOG.md',
+                github: 'kendo-labs/angular-kendo',
+                version: grunt.file.readJSON('bower.json').version
+            }
         }
-      },
-      dist: {
-        src: ['src/main.js',
-              'src/services/widget.js',
-              'src/services/kendoDecorator.js',
-              'src/factories/widget.js',
-              'src/factories/directive.js',
-              'src/directives/widgets.js',
-              'src/directives/dataSource.js',
-              'src/directives/kendoGrid.js'],
-        dest: 'build/angular-kendo.js'
-      },
+    });
 
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'build/angular-kendo.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-    // docco: {
-    //   build: {
-    //     src: [ '<%= pkg.name %>.js' ],
-    //     options: {
-    //       css: 'app/css/docco.css',
-    //       output: 'docs/'
-    //     }
-    //   }
-    // },
-    copy: {
-      main: {
-        files: [
-          { src: ['docs/angular-kendo.html'], dest: 'app/partials/docs.html', filter: 'isFile' }, // includes files in path
-        ]
-      }
-    }
-  });
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.loadNpmTasks('grunt-docco');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-conventional-changelog');
 
-  // Default task(s).
-  grunt.registerTask('default', ['concat', 'uglify']);
+    // Default task(s).
+    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('debug', ['default', 'watch']);
 
-};
+}; 
