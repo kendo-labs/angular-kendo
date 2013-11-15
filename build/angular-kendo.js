e30a9ca3585b6a687e322ce60de470a08cd6370d
Burke Holland <burkeholland@gmail.com>
2013-11-15 15:39:00 -0600

    build files

--- build/angular-kendo.js
+++ build/angular-kendo.js
@@ -1,4 +1,4 @@
-'use strict';
+(function(angular) {'use strict';
 
 // declare all the module
 angular.module('kendo.directives', []);
@@ -204,6 +204,10 @@ angular.module('kendo.directives').factory('directiveFactory', ['widgetFactory',
         }
       }
     }
+	
+	// $timeout tracking
+	var $timeoutPromise = null;
+	var unsetTimeoutPromise = function() { $timeoutPromise = null };
 
     var create = function(kendoWidget) {
 
@@ -225,12 +229,18 @@ angular.module('kendo.directives').factory('directiveFactory', ['widgetFactory',
         link: function(scope, element, attrs, ngModel) {
 
           var widget;
+		  
+		  // Instead of having angular digest each component that needs to be setup
+		  // Use the same timeout until the timeout has been executed, this will cause all
+		  //   directives to be evaluated in the next cycle, instead of over multiple cycles.
+		  if (!$timeoutPromise)
+		    $timeoutPromise = $timeout(unsetTimeoutPromise);
 
           // Bind kendo widget to element only once interpolation on attributes is done.
           // Using a $timeout with no delay simply makes sure the function will be executed next in the event queue
           // after the current $digest cycle is finished. Other directives on the same element (select for example)
           // will have been processed, and interpolation will have happened on the attributes.
-          $timeout( function() {
+          $timeoutPromise.then( function() {
 
             // create the kendo widget and bind it to the element.
             widget = widgetFactory.create(scope, element, attrs, kendoWidget);
@@ -311,49 +321,56 @@ angular.module('kendo.directives').factory('directiveFactory', ['widgetFactory',
 
 
 // ## The kendoSource directive allows setting the Kendo UI DataSource of a widget directly from the HTML.
-angular.module('kendo.directives').directive('kDataSource', [function() {
-
-  // Transforms the object into a Kendo UI DataSource.
-  var toDataSource = function(ds) {
-    // TODO: if ds is a $resource, wrap it in a kendo dataSource using an injected service
-    return kendo.data.DataSource.create(ds);
-  };
-
-  var widgetInstance = function($element) {
-    return kendo.widgetInstance($element, kendo.ui) || kendo.widgetInstance($element, kendo.dataviz.ui);
-  };
-
+angular.module('kendo.directives').directive('kDataSource', [function(){
   return {
     // This is an attribute directive
     restrict: 'A',
-    controller: ['$scope', '$attrs', '$element', function($scope, $attrs, $element) {
-
-      // can we intercept the data source
-      var ds = $scope.$eval($attrs.kDataSource);
+    controller: ['$scope', '$attrs', '$element', function($scope, $attrs, $element){
+      var widgetType = getWidgetType($attrs);
+      var dataSourceType = getDataSourceType(widgetType);
 
       // Set $kendoDataSource in the element's data. 3rd parties can define their own dataSource creation
       // directive and provide this data on the element.
-      $element.data('$kendoDataSource', toDataSource(ds));
+      $element.data('$kendoDataSource', toDataSource($scope.$eval($attrs.kDataSource), dataSourceType));
 
-      // if this data source is set to an array, we need to add a watch to keep the widget in sync
-      if ($.isArray(ds)) {
-        // add a watch to the array (value comparison: true)
-        $scope.$watch($attrs.kDataSource, function(oldVal, newVal) {
-          // if the array values differ
-          if (oldVal !== newVal) {
-            // get a reference to the widget
-            var widget = widgetInstance($element);
-            // if the widget exists and has a dataSource
-            if (widget && widget.dataSource) {
-              // read the data again which updates the widget
-              widget.dataSource.read();
-            }
-          }
-        }, true);
-      }
+      // Keep the element's data up-to-date with changes.
+      $scope.$watch($attrs.kDataSource, function(newDataSource, oldDataSource){
+        if(newDataSource !== oldDataSource){
+          $element.data('$kendoDataSource', toDataSource(newDataSource, dataSourceType));
+        }
+      });
     }]
   };
 
+  // Returns the DataSource type based on the widgetType
+  function getDataSourceType(widgetType){
+    var hierarchicalDataSourceWidgets = ['TreeView'];
+    var schedulerDataSourceWidgets = ['Scheduler'];
+        if(jQuery.inArray(widgetType, hierarchicalDataSourceWidgets) !== -1){
+      return 'HierarchicalDataSource';
+    }
+    else if(jQuery.inArray(widgetType, schedulerDataSourceWidgets) !== -1){
+      return 'SchedulerDataSource';
+    }
+    else{
+      return 'DataSource';
+    }
+  }
+
+  // Returns the widgetType, eg: 'TreeView'
+  function getWidgetType(attributes){
+    for(var attribute in attributes){
+      if(attributes.hasOwnProperty(attribute) && attribute.match(/kendo/)){
+        return attribute.replace('kendo', '');
+      }
+    }
+  }
+
+  // Transforms the object into a Kendo UI DataSource.
+  function toDataSource(dataSource, dataSourceType){
+    // TODO: if ds is a $resource, wrap it in a kendo dataSource using an injected service
+    return kendo.data[dataSourceType].create(dataSource);
+  }
 }]);
 angular.module('kendo.directives').directive('kendoGrid', ['$compile', 'kendoDecorator', '$parse', function($compile, kendoDecorator, $parse) {
 
@@ -472,3 +489,4 @@ angular.module('kendo.directives').directive('kendoGrid', ['$compile', 'kendoDec
   };
 
 }]);
+}(angular));
