94761ce96432b82e806a0e188d6c6abb46e0dae0
Mihai Bazon <mihai@bazon.net>
2013-12-17 13:17:04 +0200

    Update Gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -5,30 +5,12 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        banner: "(function(angular) {",
-        footer: "}(angular));"
-      },
-      dist: {
-        src: [
-            'src/main.js',
-            'src/services/widget.js',
-            'src/services/kendoDecorator.js',
-            'src/factories/widget.js',
-            'src/factories/directive.js',
-            'src/directives/widgets.js',
-            'src/directives/dataSource.js',
-            'src/directives/kendoGrid.js'],
-        dest: 'build/angular-kendo.js'
-      },
-    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'build/angular-kendo.js',
+        src: [ 'angular-kendo.js' ],
         dest: 'build/<%= pkg.name %>.min.js'
       }
     },
@@ -43,7 +25,7 @@ module.exports = function(grunt) {
     },
     watch: {
       scripts: {
-        files: "src/**/*.js",
+        files: "angular-kendo.js",
         tasks: ['default']
       }
     },
@@ -57,19 +39,13 @@ module.exports = function(grunt) {
 
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
   grunt.loadNpmTasks('grunt-contrib-copy');
-
-  grunt.loadNpmTasks('grunt-contrib-concat');
-
   grunt.loadNpmTasks('grunt-contrib-watch');
-
   grunt.loadNpmTasks('grunt-conventional-changelog');
 
   // Default task(s).
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['uglify']);
   grunt.registerTask('debug', ['default', 'watch']);
 
 };
