7a1b7ef9cd837670ef089c2299ec259eeb9d51b1
Mihai Bazon <mihai@bazon.net>
2014-01-15 15:56:43 +0200

    Disable AngularJS's built-in inputDirective for Kendo widgets.
    Fix #135.
    Fix #152.

--- angular-kendo.js
+++ angular-kendo.js
@@ -3,6 +3,37 @@
   var module = angular.module('kendo.directives', []),
                parse, timeout, compile, log;
 
+  // The following disables AngularJS built-in directives for <input> fields
+  // when a Kendo widget is defined.  The reason we have to do this is:
+  //
+  // 1. user updates field.
+  //
+  // 2. widget triggers "change" event on the Widget object => angular-kendo
+  //    gets notified, updates the model with the correct value!
+  //
+  // 3. widget triggers "change" event on the <input> field => AngularJS's
+  //    built-in directive validates the *content* of the input field and
+  //    updates the model again WITH THE WRONG VALUE.
+  //
+  // https://github.com/kendo-labs/angular-kendo/issues/135
+  // https://github.com/kendo-labs/angular-kendo/issues/152
+  module.config([ "$provide", function($provide){
+    $provide.decorator("inputDirective", function($delegate){
+      var orig_compile = $delegate[0].compile;
+      $delegate[0].compile = function(element, attrs) {
+        for (var i in attrs) {
+          if (attrs.hasOwnProperty(i)) {
+            if (/^kendo/.test(i) && typeof $.fn[i] == "function") {
+              return;           // HA!
+            }
+          }
+        }
+        return orig_compile.apply(this, arguments);
+      };
+      return $delegate;
+    });
+  }]);
+
   var factories = {
 
     dataSource: (function() {
