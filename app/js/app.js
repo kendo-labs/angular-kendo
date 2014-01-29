8663aaa74cecdcc27f06658257561b14a784572d
Mihai Bazon <mihai@bazon.net>
2014-01-29 11:50:31 +0200

    Add ngRoute dep.

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-angular.module('SampleApp', [ 'kendo.directives' ]).config(['$routeProvider', function($routeProvider) {
+angular.module('SampleApp', [ 'kendo.directives', 'ngRoute' ]).config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/', { templateUrl: 'app/partials/home.html',   controller: HomeCtrl }).
       otherwise({redirectTo: '/'});
